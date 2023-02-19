cdedcf39e9734cd314e628f151191875e9e8cf29
Claire Wen <clairewen@berkeley.edu>
2023-02-19 14:56:20 -0800

    bad-* tests

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -135,6 +135,12 @@ static void page_fault(struct intr_frame* f) {
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+//   f->esp -= 4;
+//   int* sp = (int*) f->esp;
+//   *sp = f->error_code;
+//   printf("testing qowieurlasdjflsajdflkajdflskadjf\n");
+   printf("%s: exit(%d)\n", thread_current()->pcb->process_name, -1);
+
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
