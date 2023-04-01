b2d136c693867b7a88dc7a863287600e2c6d2bb1
Claire Wen <clairewen@berkeley.edu>
2023-03-31 19:03:36 -0700

    revamp vampire vampires are bigggggggg we love narwhals

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -30,7 +30,7 @@ struct process {
   struct thread* main_thread;     /* Pointer to main thread */
   struct process_status* my_own;  /* Stores the current process's process_status */
   struct list children;           /* Pintos list of children's process_status */
-  struct file* fd_table[256];     /* Array of file* storing mappings from file descriptors to files */
+  struct file* fd_table[16200];     /* Array of file* storing mappings from file descriptors to files */
   int fd_index;                   /* The next available file descriptor */
 
   // USER THREADS
