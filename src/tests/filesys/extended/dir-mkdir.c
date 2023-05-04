1959fc314577b2e7ce951666de1e2baab1eddd6a
Claire Wen <clairewen@berkeley.edu>
2023-05-03 22:02:38 -0700

    crepevine

--- src/tests/filesys/extended/dir-mkdir.c
+++ src/tests/filesys/extended/dir-mkdir.c
@@ -7,10 +7,6 @@
 void test_main(void) {
   CHECK(mkdir("a"), "mkdir \"a\"");
   CHECK(create("a/b", 512), "create \"a/b\"");
-  CHECK(create("c", 512), "create \"c\"");
   CHECK(chdir("a"), "chdir \"a\"");
-  CHECK(chdir(".."), "chdir \"..\"");
-  open("c");
-  // CHECK(open("c") > 1, "open \"c\"");
-  // CHECK(open("b") > 1, "open \"b\"");
+  CHECK(open("b") > 1, "open \"b\"");
 }
